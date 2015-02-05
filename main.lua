be4d50d3193ecb4af3dfefd2aea9b74975cd0af9
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2015-02-05 15:56:09 -0800

    i added pause, pfft cuz why not. and i added images made by muffin i will use for my blocks

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ nextblock = math.random(1,7)
 --Screen stuff
 titleScreen = false
 Play = true
+pause = false
 
 -- counters for the speed of the blocks
 blockCounterAutoD = 0
@@ -11,7 +12,7 @@ blockCounterRL = 0
 blockCounterD = 0
 
 -- the block that is currently being contoled
-activeBlock = {}
+activeBlock = {x = 200, y = 200, speed = 300, img = nil}
 activeBlockTF = true
 
 -- resting blocks
@@ -28,57 +29,72 @@ function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
          y2 < y1+h1
 end
 
+function love.keyreleased(key)
+   if key == "p" then
+      if pause == false then
+			pause = true
+		else
+			pause = false
+		end
+   end
+end
+
+
 function love.update( dt )
 	-- I always start with an easy way to exit the game
 	if love.keyboard.isDown('escape') then
 		love.event.push('quit')
 	end
 
-	-- if activeBlockTF == true then
-	-- 	if nextblock == 1 then
-	-- 	table.insert(activeBlock, )
-	-- sets the speed and snapping motion of the block automaticly falling down
-	if blockCounterAutoD > 60 then
-		blockCounterAutoD = 0
-		if activeBlock.y < 510 then
-			activeBlock.y = activeBlock.y + 30
-		end
-	else
-		blockCounterAutoD = blockCounterAutoD + 100 * dt
-	end
-	-- sets the speed of the snapping for the down button, the main point was to use dt
-	if blockCounterD > 1 then
-		if love.keyboard.isDown("down", "s") then
+	if Play == true and pause == false then
+
+		-- if activeBlockTF == true then
+		-- 	if nextblock == 1 then
+		-- 	table.insert(activeBlock, )
+
+		-- sets the speed and snapping motion of the block automaticly falling down
+		if blockCounterAutoD > 60 then
+			blockCounterAutoD = 0
 			if activeBlock.y < 510 then
 				activeBlock.y = activeBlock.y + 30
-				blockCounterD = 0
+			end
+		else
+			blockCounterAutoD = blockCounterAutoD + 100 * dt
+		end
+		-- sets the speed of the snapping for the down button, the main point was to use dt
+		if blockCounterD > 1 then
+			if love.keyboard.isDown("down", "s") then
+				if activeBlock.y < 510 then
+					activeBlock.y = activeBlock.y + 30
+					blockCounterD = 0
+				end
+			end
+		else 
+			blockCounterD = blockCounterD + 100 * dt
+		end
+		-- will git rid of this one
+		if love.keyboard.isDown("w", "up") then
+			if activeBlock.y > 30 then
+				activeBlock.y = activeBlock.y - 30
 			end
 		end
-	else 
-		blockCounterD = blockCounterD + 100 * dt
-	end
-	-- will git rid of this one
-	if love.keyboard.isDown("w", "up") then
-		if activeBlock.y > 30 then
-			activeBlock.y = activeBlock.y - 30
-		end
-	end
-	-- sets the speed of the snapping for left and right, as well as the bottons to move left and right
-	if blockCounterRL > 10 then
-		if love.keyboard.isDown("left", "a") then
-			if activeBlock.x > 100 then
-				activeBlock.x = activeBlock.x - 30
-				blockCounterRL = 0
+		-- sets the speed of the snapping for left and right, as well as the bottons to move left and right
+		if blockCounterRL > 10 then
+			if love.keyboard.isDown("left", "a") then
+				if activeBlock.x > 100 then
+					activeBlock.x = activeBlock.x - 30
+					blockCounterRL = 0
+				end
 			end
-		end
-		if love.keyboard.isDown("right", "d") then
-			if activeBlock.x < 340 then
-				activeBlock.x = activeBlock.x + 30
-				blockCounterRL = 0
+			if love.keyboard.isDown("right", "d") then
+				if activeBlock.x < 340 then
+					activeBlock.x = activeBlock.x + 30
+					blockCounterRL = 0
+				end
 			end
+		else 
+			blockCounterRL = blockCounterRL + 100 * dt
 		end
-	else 
-		blockCounterRL = blockCounterRL + 100 * dt
 	end
 end
 
@@ -94,6 +110,12 @@ function love.draw( dt )
 		-- active block
 		love.graphics.setColor(255, 0, 0, 255)
 		love.graphics.rectangle("fill", activeBlock.x, activeBlock.y, 60, 60)
+		if pause == true then
+			love.graphics.setColor(255, 5, 255, 255)
+			love.graphics.rectangle("fill", 100, 30, 300, 540)
+			love.graphics.setColor(0, 0, 0, 255)
+			love.graphics.print("paused", 200, 227)
+		end
 	end
 	love.graphics.print(nextblock)
 end
