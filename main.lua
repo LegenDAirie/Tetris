4d4e3a773f79e3b33514406184347ef2285a6393
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2015-02-05 19:15:07 -0800

    i made the shape rotate, or apear to be at least

--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ pause = false
 blockCounterAutoD = 0
 blockCounterRL = 0
 blockCounterD = 0
+blockCounterRotate = 0
 
 -- the block that is currently being contoled
 activeBlock = {}
@@ -18,20 +19,10 @@ block1 = {}
 -- block3 = {}
 -- block4 = {}
 activeBlockTF = true
+StraightBlock1 = true
 
 -- resting blocks
 restingBlocks = {}
-
--- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
--- Returns true if two boxes overlap, false if they don't
--- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
--- x2,y2,w2 & h2 are the same, but for the second box
--- function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
---   return x1 < x2+w2 and
---          x2 < x1+w1 and
---          y2 < y1+h
---          y1 < y2+h2 and1
--- end
 
 function love.keyreleased(key)
    if key == "p" then
@@ -54,77 +45,91 @@ function love.update( dt )
 	end
 
 	if Play == true and pause == false then
-
-		-- for i,block1 in ipairs(activeBlock) do
-			-- sets the speed and snapping motion of the block automaticly falling down
-			if blockCounterAutoD > 60 then
-				blockCounterAutoD = 0
-				for i,blocks in ipairs(activeBlock) do
-					if blocks.y < 540 then
-					blocks.y = blocks.y + 30
-					end
+		-- sets the speed and snapping motion of the block automaticly falling down
+		if blockCounterAutoD > 60 then
+			blockCounterAutoD = 0
+			for i,blocks in ipairs(activeBlock) do
+				if blocks.y < 540 then
+				blocks.y = blocks.y + 30
 				end
-			else
-				blockCounterAutoD = blockCounterAutoD + 100 * dt
 			end
-			-- sets the speed of the snapping for the down button, the main point was to use dt
-			if blockCounterD > 1 then
-				
-				if love.keyboard.isDown("down", "s") then
-					blockCounterD = 0
-					for i,v in ipairs(activeBlock) do
-						if v.y < 540 then
-							v.y = v.y + 30
-						end
-					end
-				end
-			else 
-				blockCounterD = blockCounterD + 100 * dt
-			end
-			-- will git rid of this one
-			if love.keyboard.isDown("w", "up") then
+		else
+			blockCounterAutoD = blockCounterAutoD + (100 * dt)
+		end
+		-- sets the speed of the snapping for the down button, the main point was to use dt
+		if blockCounterD > 1 then
+			if love.keyboard.isDown("down", "s") then
+				blockCounterD = 0
 				for i,v in ipairs(activeBlock) do
-					if v.y > 30 then
-						v.y = v.y - 30
+					if v.y < 540 then
+						v.y = v.y + 30
 					end
 				end
 			end
-			-- sets the speed of the snapping for left and right, as well as the bottons to move left and right
-			if blockCounterRL > 10 then
-				if love.keyboard.isDown("left", "a") then
-					blockCounterRL = 0
-					for i,v in ipairs(activeBlock) do
-						if v.x > 100 then
-							v.x = v.x - 30
-						end
-					end
+		else 
+			blockCounterD = blockCounterD + (100 * dt)
+		end
+		if blockCounterRotate > 10 then
+			if love.keyboard.isDown("w", "up") then
+				blockCounterRotate = 0
+				if StraightBlock1 == true then
+					StraightBlock1 = false
+					activeBlock[1].x = activeBlock[1].x + 30
+					activeBlock[1].y = activeBlock[1].y + 30
+					activeBlock[3].x = activeBlock[3].x - 30
+					activeBlock[3].y = activeBlock[3].y - 30
+					activeBlock[4].x = activeBlock[4].x - 60
+					activeBlock[4].y = activeBlock[4].y - 60
+					-- for i,v in ipairs(activeBlock) do
+					-- 	if v.y > 30 then
+					-- 		v.y = v.y - 30
+					-- 	end
+					-- end
+				else
+					StraightBlock1 = true
+					activeBlock[1].x = activeBlock[1].x - 30
+					activeBlock[1].y = activeBlock[1].y - 30
+					activeBlock[3].x = activeBlock[3].x + 30
+					activeBlock[3].y = activeBlock[3].y + 30
+					activeBlock[4].x = activeBlock[4].x + 60
+					activeBlock[4].y = activeBlock[4].y + 60
 				end
-				if love.keyboard.isDown("right", "d") then
-					blockCounterRL = 0
-					for i,v in ipairs(activeBlock) do
-						if v.x < 370 then
-							v.x = v.x + 30
-						end
-					end
-				end
-			else 
-				blockCounterRL = blockCounterRL + 100 * dt
 			end
-		-- end
+		else
+			blockCounterRotate = blockCounterRotate + (100 * dt)
+		end
+		-- sets the speed of the snapping for left and right, as well as the bottons to move left and right
+		if blockCounterRL > 10 then
+			if love.keyboard.isDown("left", "a") then
+				blockCounterRL = 0
+				for i,v in ipairs(activeBlock) do
+					if v.x > 100 then
+						v.x = v.x - 30
+					end
+				end
+			end
+			if love.keyboard.isDown("right", "d") then
+				blockCounterRL = 0
+				for i,v in ipairs(activeBlock) do
+					if v.x < 370 then
+						v.x = v.x + 30
+					end
+				end
+			end
+		else 
+			blockCounterRL = blockCounterRL + 100 * dt
+		end
 
 		if nextblock == 1 then 
-			-- for i=1,4 do
-				block1 = {x = 190, y = 30, img = StraightBlockImg}
-				table.insert(activeBlock, block1)
-				block1 = {x = 220, y = 30, img = StraightBlockImg}
-				table.insert(activeBlock, block1)
-				block1 = {x = 250, y = 30, img = StraightBlockImg}
-				table.insert(activeBlock, block1)
-				block1 = {x = 280, y = 30, img = StraightBlockImg}
-				table.insert(activeBlock, block1)
-				-- block1.x = block1.x + 30
-				-- block1.y = block1.y + 30
-			-- end
+			block1 = {x = 190, y = 30, img = StraightBlockImg}
+			table.insert(activeBlock, block1)
+			block1 = {x = 220, y = 30, img = StraightBlockImg}
+			table.insert(activeBlock, block1)
+			block1 = {x = 250, y = 30, img = StraightBlockImg}
+			table.insert(activeBlock, block1)
+			block1 = {x = 280, y = 30, img = StraightBlockImg}
+			table.insert(activeBlock, block1)
+
 			while nextblock == 1 do
 				nextblock = math.random(1,7)
 			end
