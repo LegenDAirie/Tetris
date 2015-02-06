dd5b34934bace847d09b0d2c6c911bcce8f0f34f
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2015-02-05 18:49:39 -0800

    set up a table to hold the values of the blocks, so now each shape is made up for 4 separate blocks placed in a table, and got it to behave the same as the one big block did before, well kinda

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 math.randomseed(os.time())
-nextblock = math.random(1,7)
-
+-- nextblock = math.random(1,7)
+nextblock = 1
 --Screen stuff
 titleScreen = false
 Play = true
@@ -12,7 +12,11 @@ blockCounterRL = 0
 blockCounterD = 0
 
 -- the block that is currently being contoled
-activeBlock = {x = 200, y = 200, speed = 300, img = nil}
+activeBlock = {}
+block1 = {}
+-- block2 = {}
+-- block3 = {}
+-- block4 = {}
 activeBlockTF = true
 
 -- resting blocks
@@ -22,12 +26,12 @@ restingBlocks = {}
 -- Returns true if two boxes overlap, false if they don't
 -- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
 -- x2,y2,w2 & h2 are the same, but for the second box
-function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
-  return x1 < x2+w2 and
-         x2 < x1+w1 and
-         y1 < y2+h2 and
-         y2 < y1+h1
-end
+-- function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+--   return x1 < x2+w2 and
+--          x2 < x1+w1 and
+--          y2 < y1+h
+--          y1 < y2+h2 and1
+-- end
 
 function love.keyreleased(key)
    if key == "p" then
@@ -39,6 +43,9 @@ function love.keyreleased(key)
    end
 end
 
+function love.load()
+	StraightBlockImg = love.graphics.newImage('assets/orange.png')
+end
 
 function love.update( dt )
 	-- I always start with an easy way to exit the game
@@ -48,52 +55,79 @@ function love.update( dt )
 
 	if Play == true and pause == false then
 
-		-- if activeBlockTF == true then
-		-- 	if nextblock == 1 then
-		-- 	table.insert(activeBlock, )
-
-		-- sets the speed and snapping motion of the block automaticly falling down
-		if blockCounterAutoD > 60 then
-			blockCounterAutoD = 0
-			if activeBlock.y < 510 then
-				activeBlock.y = activeBlock.y + 30
+		-- for i,block1 in ipairs(activeBlock) do
+			-- sets the speed and snapping motion of the block automaticly falling down
+			if blockCounterAutoD > 60 then
+				blockCounterAutoD = 0
+				for i,blocks in ipairs(activeBlock) do
+					if blocks.y < 540 then
+					blocks.y = blocks.y + 30
+					end
+				end
+			else
+				blockCounterAutoD = blockCounterAutoD + 100 * dt
 			end
-		else
-			blockCounterAutoD = blockCounterAutoD + 100 * dt
-		end
-		-- sets the speed of the snapping for the down button, the main point was to use dt
-		if blockCounterD > 1 then
-			if love.keyboard.isDown("down", "s") then
-				if activeBlock.y < 510 then
-					activeBlock.y = activeBlock.y + 30
+			-- sets the speed of the snapping for the down button, the main point was to use dt
+			if blockCounterD > 1 then
+				
+				if love.keyboard.isDown("down", "s") then
 					blockCounterD = 0
+					for i,v in ipairs(activeBlock) do
+						if v.y < 540 then
+							v.y = v.y + 30
+						end
+					end
+				end
+			else 
+				blockCounterD = blockCounterD + 100 * dt
+			end
+			-- will git rid of this one
+			if love.keyboard.isDown("w", "up") then
+				for i,v in ipairs(activeBlock) do
+					if v.y > 30 then
+						v.y = v.y - 30
+					end
 				end
 			end
-		else 
-			blockCounterD = blockCounterD + 100 * dt
-		end
-		-- will git rid of this one
-		if love.keyboard.isDown("w", "up") then
-			if activeBlock.y > 30 then
-				activeBlock.y = activeBlock.y - 30
-			end
-		end
-		-- sets the speed of the snapping for left and right, as well as the bottons to move left and right
-		if blockCounterRL > 10 then
-			if love.keyboard.isDown("left", "a") then
-				if activeBlock.x > 100 then
-					activeBlock.x = activeBlock.x - 30
+			-- sets the speed of the snapping for left and right, as well as the bottons to move left and right
+			if blockCounterRL > 10 then
+				if love.keyboard.isDown("left", "a") then
 					blockCounterRL = 0
+					for i,v in ipairs(activeBlock) do
+						if v.x > 100 then
+							v.x = v.x - 30
+						end
+					end
 				end
-			end
-			if love.keyboard.isDown("right", "d") then
-				if activeBlock.x < 340 then
-					activeBlock.x = activeBlock.x + 30
+				if love.keyboard.isDown("right", "d") then
 					blockCounterRL = 0
+					for i,v in ipairs(activeBlock) do
+						if v.x < 370 then
+							v.x = v.x + 30
+						end
+					end
 				end
+			else 
+				blockCounterRL = blockCounterRL + 100 * dt
 			end
-		else 
-			blockCounterRL = blockCounterRL + 100 * dt
+		-- end
+
+		if nextblock == 1 then 
+			-- for i=1,4 do
+				block1 = {x = 190, y = 30, img = StraightBlockImg}
+				table.insert(activeBlock, block1)
+				block1 = {x = 220, y = 30, img = StraightBlockImg}
+				table.insert(activeBlock, block1)
+				block1 = {x = 250, y = 30, img = StraightBlockImg}
+				table.insert(activeBlock, block1)
+				block1 = {x = 280, y = 30, img = StraightBlockImg}
+				table.insert(activeBlock, block1)
+				-- block1.x = block1.x + 30
+				-- block1.y = block1.y + 30
+			-- end
+			while nextblock == 1 do
+				nextblock = math.random(1,7)
+			end
 		end
 	end
 end
@@ -108,8 +142,16 @@ function love.draw( dt )
 		love.graphics.setColor(255, 255, 255, 255)
 		love.graphics.rectangle("fill", 100, 30, 300, 540)
 		-- active block
-		love.graphics.setColor(255, 0, 0, 255)
-		love.graphics.rectangle("fill", activeBlock.x, activeBlock.y, 60, 60)
+		for i,blocks in ipairs(activeBlock) do
+			love.graphics.draw(blocks.img, blocks.x, blocks.y)
+			-- love.graphics.print("block1 x: " .. blocks.x .. "block1 y: " .. blocks.y)
+		end
+		-- love.graphics.setColor(255, 0, 0, 255)
+		-- love.graphics.rectangle("fill", activeBlock.x, activeBlock.y, 60, 60)
+		for i,v in ipairs(activeBlock) do
+			print("x; " .. v.x .. "y; " .. v.y)
+		end
+
 		if pause == true then
 			love.graphics.setColor(255, 5, 255, 255)
 			love.graphics.rectangle("fill", 100, 30, 300, 540)
