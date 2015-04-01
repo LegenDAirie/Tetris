81b2a23d747f66bc1527e445ef0f34f06c0eeb20
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2015-03-31 20:24:20 -0700

    cool, i finally got a block set to attach to blocks so blocks dont move on their own but instead follow the block set. so hopfully i can just manipulate the bock set and they will follow.

--- MovingBlock.lua
+++ MovingBlock.lua
@@ -3,39 +3,106 @@ Block.__index = Block
 
 
 
-function Block.create()
-	local self = {}
-	setmetatable(self,Block)
-	self.xPix = 0
-	self.yPix = 0
-	self.xCoor = 0
-	self.yCoor = 0
-	self.moving = true
-	return self
-end
+-- function Block.create()
+-- 	local self = {}
+-- 	setmetatable(self,Block)
+-- 	self.xPix = 0
+-- 	self.yPix = 0
+-- 	self.xCoor = 0
+-- 	self.yCoor = 0
+-- 	self.moving = true
+-- 	return self
+-- end
 
 function Block:update(dt)
 	-- falling speed needs to be 1/9th per sec
-	self.yPix = self.yPix + 20 * (dt * 2) -- dt is in seconds
+	-- self.yPix = self.yPix + 20 * (dt * 2) -- dt is in seconds
+	-- self.yCoor = math.floor(self.yPix / 20)
+	self.yCoor = self.yCoor - self.offSetY
+	self.xCoor = self.xCoor - self.offSetx
+	self.yCoor = math.floor(BlockSet.yPix / 20) + self.offSetY
+	self.xCoor = math.floor(BlockSet.xPix / 20) + self.offSetx
+	-- print("self.yCoor is " .. self.yCoor)
+end
 
-	--[[ this translates the x and y positions from pixels to my own
+function updateSet(dt)
+		--[[ this translates the x and y positions from pixels to my own
 	coordinates so i can make sure the they always stay in the proper
 	region --]]
-	self.yCoor = math.floor(self.yPix / 20)
-	print("self.yCoor is " .. self.yCoor)
+	BlockSet.yPix = BlockSet.yPix + 20 * (dt * 2)
+	BlockSet.yCoor = math.floor(BlockSet.yPix / 20)
+	-- print("BlockSet.yCoor is " .. BlockSet.yCoor)
 end
 
 function Block:draw()
 	-- love.graphics.draw(imgBlock,self.x, self.y)
+	love.graphics.setColor(100, 23,213, 255)
 	love.graphics.rectangle("fill", self.xCoor * 20, self.yCoor * 20, 20, 20)
+end
+
+function drawSet()
+	love.graphics.setColor(25,50,75,255)
+	love.graphics.rectangle("fill", BlockSet.xCoor * 20, BlockSet.yCoor * 20, 80, 60)
 end
 
 function spawnBlocks()
 	if rdyForNewMovBlock == true then
-		table.insert(movingBlocks, Block.create())
+		-- table.insert(movingBlocks, Block.create())
+		createBlockSet()
+		Block.create()
 		rdyForNewMovBlock = false
 	end
 end
 
-function love.keypressed(key,unicode)
+function createBlockSet()
+	BlockSet = {}
+	BlockSet.xCoor = 0
+	BlockSet.yCoor = 0
+	BlockSet.xPix = 20 * BlockSet.xCoor
+	BlockSet.yPix = 20 * BlockSet.yCoor
+	return BlockSet
+end
+
+function Block.create()
+	if thisBlock == 1 then
+		local self = {}
+		setmetatable(self,Block)
+		self.xCoor = 0
+		self.yCoor = 0
+		self.offSetY = self.yCoor
+		self.offSetx = self.xCoor
+		-- self.xPix = 20 * self.xCoor
+		-- self.yPix = 20 * self.yCoor
+		table.insert(movingBlocks, self)
+
+		local self = {}
+		setmetatable(self,Block)
+		self.xCoor = 1
+		self.yCoor = 0
+		self.offSetY = self.yCoor
+		self.offSetx = self.xCoor
+		-- self.xPix = 20 * self.xCoor
+		-- self.yPix = 20 * self.yCoor
+		table.insert(movingBlocks, self)
+
+		local self = {}
+		setmetatable(self,Block)
+		self.xCoor = 1
+		self.yCoor = 1
+		self.offSetY = self.yCoor
+		self.offSetx = self.xCoor
+		-- self.xPix = 20 * self.xCoor
+		-- self.yPix = 20 * self.yCoor
+		table.insert(movingBlocks, self)
+
+		local self = {}
+		setmetatable(self,Block)
+		self.xCoor = 2
+		self.yCoor = 1
+		self.offSetY = self.yCoor
+		self.offSetx = self.xCoor
+		-- self.xPix = 20 * self.xCoor
+		-- self.yPix = 20 * self.yCoor
+		table.insert(movingBlocks, self)
+	end
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
 require("movingBlock")
+require("rotate")
 function love.load()
+	math.randomseed(os.time())
 	restart()
 	loadResources()
 end
 
 function restart()
 	movingBlocks = {}
+	thisBlock = 1
 	rdyForNewMovBlock = true
 end
 function love.update(dt)
@@ -13,15 +16,33 @@ function love.update(dt)
 end
 
 function updateGame(dt)
+	-- spawns the block set
 	spawnBlocks()
-	movingBlocks[1]:update(dt)
+	-- updates the block set
+	updateSet(dt)
+	for i,b in ipairs(movingBlocks) do
+		b:update(dt)
+	end
+	-- movingBlocks[1]:update(dt)
 end
 
 function love.draw()
-	movingBlocks[1]:draw()
+
+	-- draws the block set
+	-- movingBlocks[1]:draw()
+	drawSet()
+	for i,b in ipairs(movingBlocks) do
+		b:draw()
+	end
 end
 
 function loadResources()
 	-- Load images
 	imgBlock = love.graphics.newImage("assets/Pieces.png")
+end
+
+function love.keypressed(key,unicode)
+	if key == 'up' then
+		-- rotate()
+	end
 end
